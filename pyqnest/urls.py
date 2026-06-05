1df7c33241a2e531f33f08ad489f6c50a8dde227
pachuau10 <duhteapachuau10@gmail.com>
2026-06-05 16:09:27 +0530

    Add /favicon.ico redirect to static SVG

--- pyqnest/urls.py
+++ pyqnest/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from papers import views as papers_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/pyqnest_icon_1024.svg', permanent=True)),
     path('robots.txt', papers_views.robots_txt, name="robots_txt"),
     path('sitemap.xml', papers_views.sitemap_xml, name="sitemap"),
     path('', include('papers.urls')),
